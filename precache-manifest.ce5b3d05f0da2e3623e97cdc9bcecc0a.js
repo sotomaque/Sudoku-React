ccbd7a2d7fb8993ae0ea025be84a756a302af6da
sotomaque <sotomaque@gmail.com>
2020-05-06 20:40:47 -0700

    Updates

---
+++ precache-manifest.ce5b3d05f0da2e3623e97cdc9bcecc0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e713dbb8bc435b72c4f8181d28812bf0",
+    "url": "/Sudoku-React/index.html"
+  },
+  {
+    "revision": "1c69c1163fcfbb967816",
+    "url": "/Sudoku-React/static/css/2.bc9e6f51.chunk.css"
+  },
+  {
+    "revision": "bc1fb8519264f58a02c1",
+    "url": "/Sudoku-React/static/css/main.3399893e.chunk.css"
+  },
+  {
+    "revision": "1c69c1163fcfbb967816",
+    "url": "/Sudoku-React/static/js/2.c62db413.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/Sudoku-React/static/js/2.c62db413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1fb8519264f58a02c1",
+    "url": "/Sudoku-React/static/js/main.8c5a7a20.chunk.js"
+  },
+  {
+    "revision": "f659bfedddeba4d03da0",
+    "url": "/Sudoku-React/static/js/runtime-main.f053f899.js"
+  }
+]);
